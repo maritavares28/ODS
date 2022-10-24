a17c2d3fd916c28f7d8367f102b71180bcded09e
aaa <violoncelistamariafernanda@gmail.com>
2022-10-24 08:41:43 -0300

    código ods 1

--- app/src/main/java/com/example/ods/ODS1Activity.java
+++ app/src/main/java/com/example/ods/ODS1Activity.java
@@ -1,13 +1,35 @@
-package com.example.ods;
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
-public class ODS1Activity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
+    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn10,btn11,btn12,btn13,btn14,btn15,btn16,btn17;
+}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ods1);
+
+        //botão sair
+
+btnSair = findViewById(R.id.btnSair);
+btnSair.setOnClickListener(new View.OnClickListener(){
+
+    @Override
+    public void onClicl(View view){
+        finish();
+        finishAffinity();
     }
-}
+
+}};
+
+
+btn1 = findViewById(R.id.btn1);
+btn1.setOnClickListenner(new View.OnClickListener(){
+    @Override
+    public void onClick(View view){
+        intent ODS1 = new intent(getApplicationContext(),ODS1Activity.class);
+        startActivity(ods1);
+        }
+        }};
