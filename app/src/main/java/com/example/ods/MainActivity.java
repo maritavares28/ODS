6c7030174b4f8f22bf0911c0d5a6dabb15c00b10
aaa <violoncelistamariafernanda@gmail.com>
2022-10-24 09:40:23 -0300

    código main

--- app/src/main/java/com/example/ods/MainActivity.java
+++ app/src/main/java/com/example/ods/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.ods;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    // declarar os botoes
+    Button btn1,btn2,btn3,btn4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //ODS1
+        btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent telaODS1 = new Intent( getApplicationContext() , ODS1Activity.class);
+                startActivity(telaODS1);
+            }
+        });
+        //ODS2
+        btn2 = findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent telaOD2 = new Intent( getApplicationContext(), ODS2Activity.class);
+                startActivity(telaOD2);
+            }
+        });
+        //ODS3
+
     }
 }
--- app/src/main/java/com/example/ods/ODS1Activity.java
+++ app/src/main/java/com/example/ods/ODS1Activity.java
@@ -1,35 +1,19 @@
-import android.os.Bundle;
+package com.example.ods;
+
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn10,btn11,btn12,btn13,btn14,btn15,btn16,btn17;
-}
+public class ODS1Activity extends AppCompatActivity {
+
+    Button btnVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ods1);
+        setContentView(R.layout.activity_ods2);
 
-        //botão sair
+        btnVoltar = findViewById(R.id.btnVoltar);
 
-btnSair = findViewById(R.id.btnSair);
-btnSair.setOnClickListener(new View.OnClickListener(){
-
-    @Override
-    public void onClicl(View view){
-        finish();
-        finishAffinity();
     }
-
-}};
-
-
-btn1 = findViewById(R.id.btn1);
-btn1.setOnClickListenner(new View.OnClickListener(){
-    @Override
-    public void onClick(View view){
-        intent ODS1 = new intent(getApplicationContext(),ODS1Activity.class);
-        startActivity(ods1);
-        }
-        }};
+}
