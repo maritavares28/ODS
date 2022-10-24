f78032289e0be7663a3a4cd3a6f665997b3c627b
aaa <violoncelistamariafernanda@gmail.com>
2022-10-24 09:59:47 -0300

    mais código

--- app/src/main/java/com/example/ods/MainActivity.java
+++ app/src/main/java/com/example/ods/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     // declarar os botoes
-    Button btn1,btn2,btn3,btn4;
+    Button btn1,btn2,btn3,btn4,btn5,bt6,btn7,btn8,btn9,btn10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +28,34 @@ public class MainActivity extends AppCompatActivity {
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent telaOD2 = new Intent( getApplicationContext(), ODS2Activity.class);
-                startActivity(telaOD2);
+                Intent telaODS2 = new Intent( getApplicationContext(), ODS2Activity.class);
+                startActivity(telaODS2);
             }
         });
         //ODS3
+        btn3 = findViewById(R.id.btn3);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent telaODS3 = new Intent(getApplicationContext(),ODS3Activity.class);
+                startActivity(telaODS3);
+
+            }
+        });
+        //ODS4
+        btn4 = findViewById(R.id.btn4);
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent telaODS4 = onNewIntent(getApplicationContext(),ODS4Activity.class);
+                startActivity(telaODS4);
+            }
+
+        });
+        //ODS5
+        btn5 = findViewById(R.id.btn5);
+        
+
 
     }
 }
